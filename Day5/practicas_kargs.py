1f07564273edc5153fddd5e607c0df07b6b61adc
Falitomal <falitomal@gmail.com>
2023-02-28 17:27:44 +0100

    kwargs practice

---
+++ Day5/practicas_kargs.py
@@ -0,0 +1,14 @@
+#funcion cantidad de atributos que ponga la cantidad de atributos y devuelva como resultado
+
+def cantidad_atributos(**kwargs):
+	return len(kwargs)
+
+#funcion llama lista_atributos que duevuelva una lista con los atributos en forma de palabras clave
+def lista_atributos(**kwargs):
+	return list(kwargs.values())
+
+#funcion llamada describir_persona, que tome su nombre como parametro y una serie indeterminado de argumentos
+def describir_persona(nombre, **kwargs):
+    print(f"Características de {nombre}:")
+    for arg in kwargs:
+      print(f"{arg}: {kwargs[arg]}")
