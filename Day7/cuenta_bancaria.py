5e62ab167bec76e112777828fac6fb374817f5f6
falitomal <falitomal@gmail.com>
2023-03-18 13:05:01 +0100

    cuenta bancaria y mirando proyects

--- Day7/cuenta_bancaria.py
+++ Day7/cuenta_bancaria.py
@@ -7,21 +7,24 @@ class Persona:
         return f"Nombre: {self.nombre}, apellido: {self.apellido}"
 
 class Cliente(Persona):
-    def __init__(self, nombre, apellido, numero_cuenta, balance):
+    def __init__(self, nombre, apellido, numero_cuenta, balance = 0):
         super().__init__(nombre, apellido)
         self.numero_cuenta = numero_cuenta
         self.balance = balance
 
     def __str__(self):
-        return f"Nombre: {self.nombre}, apellido: {self.apellido}, numero de cliente: {self.numero_cuenta}, balance: {self.balance}"
+        return f"Nombre: {self.nombre}, apellido: {self.apellido}\n numero de cliente: {self.numero_cuenta}, balance: {self.balance}"
     
     def depositar(self, money):
         self.balance += money
         print(f"Se ha depositado {money} en la cuenta {self.numero_cuenta}")
     
-    def retirar(self, money):
-        self.balance -= money
-        print(f"Se ha retirado {money} de la cuenta {self.numero_cuenta}")
+    def retirar(self, money_retired):
+        if self.balance >= money_retired:
+            self.balance -= money_retired
+            print(f"Se ha retirado {money_retired} de la cuenta {self.numero_cuenta}")
+        else:
+            print(f"\nNo hay suficiente dinero, este es tu saldo: {self.balance}\n")
 
 
 def crear_cliente():
@@ -32,11 +35,25 @@ def crear_cliente():
     return Cliente(nombre, apellido, numero_cuenta, balance)
 
 def inicio():
-    print("Bienvenido al banco")
-    print("1. Crear cliente")
-    print("2. Depositar")
-    print("3. Retirar")
-    print("4. Salir")
-    opcion = int(input("Ingrese la opcion: "))
-    return opcion
+    mi_cliente = crear_cliente()
+    opcion = 0
+    while opcion != 3:
+        print("+" * 10)
+        print("Bienvenido al banco")
+        print("+" * 10)
+        print(mi_cliente)
+        print("+" * 10)
+        print("1. Depositar")
+        print("2. Retirar")
+        print("3. Salir")
+        opcion = int(input("Ingrese la opcion: "))
+        if opcion == 1:
+            money = int(input(print("Cuanto dinero quiere ingresar: ")))
+            mi_cliente.depositar(money)
+        elif opcion == 2:
+            money = int(input(print("Cuanto dinero quiere retirar: ")))
+            mi_cliente.retirar(money)
+    print(f"Gracias {mi_cliente.nombre} por usar nuestros servicios")
+    
+inicio()
 
---
+++ Free-CodeCamp/service-api.py
@@ -0,0 +1,8 @@
+#Api para conectar con googlemaps
+#https://developers.google.com/maps/documentation/geocoding/intro   
+#https://developers.google.com/maps/documentation/geocoding/start
+#https://developers.google.com/maps/documentation/geocoding/get-api-key
+#https://developers.google.com/maps/documentation/geocoding/usage-limits
+
+import requests
+import json
