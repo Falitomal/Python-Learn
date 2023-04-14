acacd46e83b543ffe76e4c07c37837e3277bfc4d
Falitomal <falitomal@gmail.com>
2023-04-14 07:15:41 +0200

    upload unsaved

---
+++ Day8/archivo1.py
@@ -0,0 +1,14 @@
+"""
+Este es un modulo que escribe algo
+"""
+
+
+def una_funcion():
+    numero1 = 300
+    print(numero1)
+
+
+una_funcion()
+
+
+#python -m pylint archivo1.py -r y
---
+++ Day8/practica_pylint.py
@@ -0,0 +1,19 @@
+"""
+Funcion para sumar dos numeros
+"""
+
+def sumar(numer, numer2):
+    """_summary_
+
+    Args:
+        numer (_type_): _description_
+        numer2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return numer+numer2
+
+
+SUMA = sumar(5,7)
+print(SUMA)
